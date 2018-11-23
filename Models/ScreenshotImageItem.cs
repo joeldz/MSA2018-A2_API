d1a771864f5f56fea5f7d73471d56ddc9aeda289
Joel De Zoysa <joel.de.zoysa@gmail.com>
2018-11-23 19:21:54 +1300

    Remove unnecessary POST inputs

--- Models/ScreenshotImageItem.cs
+++ Models/ScreenshotImageItem.cs
@@ -8,10 +8,6 @@ namespace jdezscreenshotservice.Models
 {
     public class ScreenshotImageItem
     {
-        public string Series { get; set; }
-        public string Episode { get; set; }
-        public string Timestamp { get; set; }
-        public string Subtitle { get; set; }
         public IFormFile Image { get; set; }
     }
 }
